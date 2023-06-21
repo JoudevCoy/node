76dae86db84b1e3e000696a144754e79c2d4a9dd
JoudevCoy <joudev09@gmail.com>
2023-06-21 21:58:36 +0800

    #

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+const express = require('express')
+const app = express()
 
-const port = 3000;
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-const renderHTML = (path, res) => {
-  fs.readFile(path, (err, data) => {
-    if(err){
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
-}
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-    })
-    
-    //memasukan nilai url
-    const url = req.url;
-    
-    switch(url){
-      case '/about':
-        renderHTML('./about.html', res);
-        console.log("about");
-        break;
-      default:
-        renderHTML('./index.html', res);
-        console.log("home");
-        break;
-    }
-
-  })
-  .listen(port, () => {
-  console.log(`Server Berjalan di Port ${port}..`);
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${port}`)
 });
+
+
+
+
+// const http = require("http");
+// const url = require("url");
+// const fs = require("fs");
+
+// const port = 3000;
+
+// const renderHTML = (path, res) => {
+//   fs.readFile(path, (err, data) => {
+//     if(err){
+//     } else {
+//       res.write(data);
+//       res.end();
+//     }
+//   });
+// }
+
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, {
+//       'Content-Type': 'text/html',
+//     })
+    
+//     //memasukan nilai url
+//     const url = req.url;
+    
+//     switch(url){
+//       case '/about':
+//         renderHTML('./about.html', res);
+//         console.log("about");
+//         break;
+//       default:
+//         renderHTML('./index.html', res);
+//         console.log("home");
+//         break;
+//     }
+
+//   })
+//   .listen(port, () => {
+//   console.log(`Server Berjalan di Port ${port}..`);
+// });
