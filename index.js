821b83ef3fd2499c2950c320b8d49910d1cfa071
JoudevCoy <joudev09@gmail.com>
2023-06-21 22:15:46 +0800

    #

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/', (req, res) => {
+  res.send('Ini adalah halaman Home');
 });
+
+app.get('/about', (req, res) => {
+  res.send('Ini adalah halaman about');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
 
 
 
